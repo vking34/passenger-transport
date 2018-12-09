22021a90f157cae2c678cba1ea82a9694158bf65
vking34 <lzzvkingzzl@gmail.com>
2018-12-09 11:15:37 +0700

    merge admin page

--- src/main/java/com/hust/itss/controllers/IndexController.java
+++ src/main/java/com/hust/itss/controllers/IndexController.java
@@ -17,13 +17,11 @@ import static com.hust.itss.constants.security.RoleContants.*;
 public class IndexController {
     @GetMapping
     public String getIndex(@CookieValue(value = JWTAuthenticationService.JWT_COOKIE_NAME, required = false) String token, Model model){
-//        System.out.println("GET: index with token: " + token);
         if(token != null){
             String role = Jwts.parser().setSigningKey(SecurityContants.SECRET_KEY)
                     .parseClaimsJws(token)
                     .getBody()
                     .getAudience();
-//            System.out.println("role : " + role);
             switch (role) {
                 case ADMIN:
                     return "redirect:/admin";
@@ -55,40 +53,25 @@ public class IndexController {
     public String getAdminPage(HttpServletResponse response,
                                @CookieValue(value = JWTAuthenticationService.JWT_COOKIE_NAME, required = false) String token,
                                Model model){
-        System.out.println("GET: admin with token: " + token);
         if (token == null){
             response.setStatus(403);
         }
-
-
-
         return "admin";
     }
 
     @GetMapping("/driver")
     public String getDriverPage(@CookieValue(value = JWTAuthenticationService.JWT_COOKIE_NAME) String token, Model model){
-        System.out.println("GET: Driver with token: " + token);
         if (token == null){
             return "redirect:/login";
         }
-
-        String role = Jwts.parser().setSigningKey(SecurityContants.SECRET_KEY)
-                .parseClaimsJws(token)
-                .getBody()
-                .getAudience();
-        System.out.println("role : " + role);
-
-
         return "driver";
     }
 
     @GetMapping("/assistant")
     public String getAssistantPage(@CookieValue(value = JWTAuthenticationService.JWT_COOKIE_NAME) String token, Model model){
-        System.out.println("GET: assistant with token: " + token);
         if (token == null){
             return "redirect:/login";
         }
-
         return "assistant";
     }
 
